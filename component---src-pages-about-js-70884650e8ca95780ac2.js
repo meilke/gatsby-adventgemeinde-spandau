6e473759e6137fb0d5a6f61578d02e5a273dcaaf
meilke <christian.meilke@googlemail.com>
2020-05-08 13:46:31 +0000

    Deploying to gh-pages from  @ a57b14f5cd7d92faf1434b1af1b5fb36e4bebc0c 🚀

---
+++ component---src-pages-about-js-70884650e8ca95780ac2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return u})),t.d(e,"query",(function(){return o}));var a=t("q1tI"),r=t.n(a),l=t("Bl7J");function u(n){var e=n.data;return r.a.createElement(l.a,null,e.allContentfulFrontpage.edges.map((function(n){return r.a.createElement("div",null,r.a.createElement("h1",null,n.node.title),r.a.createElement("p",null,n.node.content.content))})))}var o="1233332398"}}]);
+//# sourceMappingURL=component---src-pages-about-js-70884650e8ca95780ac2.js.map
